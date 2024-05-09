a6b006944e3717e9c615662ed11d5285b86caf9d
lima-12 <limaufpa2022@gmail.com>
2024-05-09 12:47:04 -0300

    ajuste

--- script/script.js
+++ script/script.js
@@ -5,3 +5,18 @@ inputModoNoturno.addEventListener("click", () => {
     const modo = inputModoNoturno.checked ? "dark" : "light"
     elemento.setAttribute("data-bs-theme", modo)
 })
+
+
+// Função para fechar o menu quando um item é clicado
+document.addEventListener("DOMContentLoaded", function() {
+    const menuItems = document.querySelectorAll('.navbar-nav .nav-link');
+    menuItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+            const navbarToggle = document.querySelector('.navbar-toggler');
+            const navbarCollapse = document.querySelector('.navbar-collapse');
+            if (navbarToggle && navbarCollapse.classList.contains('show')) {
+                navbarToggle.click();
+            }
+        });
+    });
+});
